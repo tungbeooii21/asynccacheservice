2c76c2d4304510be4117ea5fa98c832c4653f603
tungbeooii21 <clacecharapun0suy@hotmail.com>
2024-05-07 20:59:31 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function findTheDifference(s, t) {
-  let result = 0;
-  for (const char of s) {
-    result ^= char.charCodeAt(0);
-  }
-  for (const char of t) {
-    result ^= char.charCodeAt(0);
-  }
-  return String.fromCharCode(result);
-}
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
